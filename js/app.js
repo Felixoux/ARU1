1d6f7b6127c1d017eef4eae88ad0611aecca4652
Felixoux <73909104+Felixoux@users.noreply.github.com>
2021-03-14 16:55:34 +0100

    save

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Deroulement effect navbar
-$liParent = document.querySelector('.header-nav li');
-$liSlide = document.querySelector('.header-nav .deroulant');
+var liParent = document.querySelector('.header-nav .deroulant');
+var liSlide = document.querySelector('.header-nav .sous');
 
-$liParent.addEventListener('mouseover', () => {
-    $liSlide.classList.add(".active");
+    liParent.addEventListener('mouseover', () => {
+    $liSlide.classList.toogle(".active");
 })
