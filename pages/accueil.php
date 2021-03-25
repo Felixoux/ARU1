379edd880defd35f7a464a13a9a1b47a9cfd298a
Felixoux <73909104+Felixoux@users.noreply.github.com>
2021-03-25 20:48:27 +0100

    Ajout Fleche de scroll

--- pages/accueil.php
+++ pages/accueil.php
@@ -134,76 +134,81 @@
                     </div> 
                 </div>   
         </section>
+        <div class="scroll-down__chevron center-mg">
+            <a href="#footer"><svg class="scroll-down__chevron" width='25' height='25' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill="#8491C7" d='M100 5.5c0 1.3-.5 2.6-1.5 3.5L50 57.5 1.5 9c-2-2-2-5.1 0-7.1s5.1-2 7.1 0L50 43.4 91.5 1.9c2-2 5.1-2 7.1 0 .9 1 1.4 2.3 1.4 3.6z'/></svg></a>
+        </div>
         <!-- Actu  -->
-        <section class="actu">  
-            <h2 class="float-l mt-2">Fil d'actualité</h2>
-            <svg class="scribe float-l m-2 mt-1" width="50" height="50"  version="1.1" baseProfile="tiny" id="Calque_1"
-	            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50"
-	            xml:space="preserve">
-                <path fill="#D4DCFF" d="M48.54,10.88H37.4V1.57C37.4,0.7,36.75,0,35.94,0H1.46C0.66,0,0,0.7,0,1.57v40.59C0,46.48,3.29,50,7.32,50
-	            h35.35c4.04,0,7.32-3.52,7.32-7.85V12.45C50,11.58,49.34,10.88,48.54,10.88z M7.32,46.86c-2.42,0-4.39-2.11-4.39-4.71V3.14h31.54
-	            c0,41.47-0.02,39.17,0.03,39.76c0.13,1.45,0.63,2.83,1.43,3.97H7.32z M47.07,42.15c0,2.6-1.97,4.71-4.39,4.71c-0.33,0-0.56,0-0.88,0
-	            c-2.42,0-4.39-2.11-4.39-4.71V14.02h9.67V42.15z"/>
-                <path fill="#D4DCFF" d="M29.69,7.53H7.81C7,7.53,6.35,8.23,6.35,9.1c0,0.87,0.66,1.57,1.46,1.57h21.88c0.81,0,1.46-0.7,1.46-1.57
-	            C31.15,8.23,30.5,7.53,29.69,7.53z"/>
-                <path fill="#D4DCFF" d="M29.69,14.23H7.81c-0.81,0-1.46,0.7-1.46,1.57c0,0.87,0.66,1.57,1.46,1.57h21.88c0.81,0,1.46-0.7,1.46-1.57
-	            C31.15,14.93,30.5,14.23,29.69,14.23z"/>
-                <path fill="#D4DCFF" d="M29.69,39.33H7.81c-0.81,0-1.46,0.7-1.46,1.57S7,42.47,7.81,42.47h21.88c0.81,0,1.46-0.7,1.46-1.57
-	            S30.5,39.33,29.69,39.33z"/>
-                <path fill="#D4DCFF" d="M29.69,20.92H18.75c-0.81,0-1.46,0.7-1.46,1.57v11.72c0,0.87,0.66,1.57,1.46,1.57h10.94
-	            c0.81,0,1.46-0.7,1.46-1.57V22.49C31.15,21.62,30.5,20.92,29.69,20.92z M28.22,32.64h-8.01v-8.58h8.01V32.64z"/>
-                <path fill="#D4DCFF" d="M7.81,26.57h4.69c0.81,0,1.46-0.7,1.46-1.57c0-0.87-0.66-1.57-1.46-1.57H7.81c-0.81,0-1.46,0.7-1.46,1.57
-	            C6.35,25.87,7,26.57,7.81,26.57z"/>
-                <path fill="#D4DCFF" d="M7.81,33.26h4.69c0.81,0,1.46-0.7,1.46-1.57c0-0.87-0.66-1.57-1.46-1.57H7.81c-0.81,0-1.46,0.7-1.46,1.57
-	            C6.35,32.56,7,33.26,7.81,33.26z"/>
-            </svg>   
-            <div class="container flex">
-                <div class="card-actu flex">
-                    <div class="img-card"></div>
-                    <div class="text-bg_card flex">
-                        <p>Ici toutes les informations
-                            sur le Covid-19. Les mesures 
-                            a prendre sont désormais 
-                            appliquées. <br><br><br>
-                            Vous allez trouver un document ci dessous 
-                        </p>
-                        <div class="line"></div>
-                    </div>          
-                    <div class="btn_more flex-center">
-                        <img class="show-more-infos" src="/img/svg/plus.svg" alt="more-infos" width="20" height="20">
+        <section class="actu" id="actu">  
+            <div class="container-carroussel container">
+                <h2 class="float-l mt-2">Fil d'actualité</h2>
+                <svg class="scribe float-l m-2 mt-1" width="50" height="50"  version="1.1" baseProfile="tiny" id="Calque_1"
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 50 50"
+                    xml:space="preserve">
+                    <path fill="#D4DCFF" d="M48.54,10.88H37.4V1.57C37.4,0.7,36.75,0,35.94,0H1.46C0.66,0,0,0.7,0,1.57v40.59C0,46.48,3.29,50,7.32,50
+                    h35.35c4.04,0,7.32-3.52,7.32-7.85V12.45C50,11.58,49.34,10.88,48.54,10.88z M7.32,46.86c-2.42,0-4.39-2.11-4.39-4.71V3.14h31.54
+                    c0,41.47-0.02,39.17,0.03,39.76c0.13,1.45,0.63,2.83,1.43,3.97H7.32z M47.07,42.15c0,2.6-1.97,4.71-4.39,4.71c-0.33,0-0.56,0-0.88,0
+                    c-2.42,0-4.39-2.11-4.39-4.71V14.02h9.67V42.15z"/>
+                    <path fill="#D4DCFF" d="M29.69,7.53H7.81C7,7.53,6.35,8.23,6.35,9.1c0,0.87,0.66,1.57,1.46,1.57h21.88c0.81,0,1.46-0.7,1.46-1.57
+                    C31.15,8.23,30.5,7.53,29.69,7.53z"/>
+                    <path fill="#D4DCFF" d="M29.69,14.23H7.81c-0.81,0-1.46,0.7-1.46,1.57c0,0.87,0.66,1.57,1.46,1.57h21.88c0.81,0,1.46-0.7,1.46-1.57
+                    C31.15,14.93,30.5,14.23,29.69,14.23z"/>
+                    <path fill="#D4DCFF" d="M29.69,39.33H7.81c-0.81,0-1.46,0.7-1.46,1.57S7,42.47,7.81,42.47h21.88c0.81,0,1.46-0.7,1.46-1.57
+                    S30.5,39.33,29.69,39.33z"/>
+                    <path fill="#D4DCFF" d="M29.69,20.92H18.75c-0.81,0-1.46,0.7-1.46,1.57v11.72c0,0.87,0.66,1.57,1.46,1.57h10.94
+                    c0.81,0,1.46-0.7,1.46-1.57V22.49C31.15,21.62,30.5,20.92,29.69,20.92z M28.22,32.64h-8.01v-8.58h8.01V32.64z"/>
+                    <path fill="#D4DCFF" d="M7.81,26.57h4.69c0.81,0,1.46-0.7,1.46-1.57c0-0.87-0.66-1.57-1.46-1.57H7.81c-0.81,0-1.46,0.7-1.46,1.57
+                    C6.35,25.87,7,26.57,7.81,26.57z"/>
+                    <path fill="#D4DCFF" d="M7.81,33.26h4.69c0.81,0,1.46-0.7,1.46-1.57c0-0.87-0.66-1.57-1.46-1.57H7.81c-0.81,0-1.46,0.7-1.46,1.57
+                    C6.35,32.56,7,33.26,7.81,33.26z"/>
+                </svg>   
+                <div class="container flex">
+                    <div class="card-actu flex">
+                        <div class="img-card"></div>
+                        <div class="text-bg_card flex">
+                            <p>Ici toutes les informations
+                                sur le Covid-19. Les mesures 
+                                a prendre sont désormais 
+                                appliquées. <br><br><br>
+                                Vous allez trouver un document ci dessous 
+                            </p>
+                            <div class="line"></div>
+                        </div>          
+                        <div class="btn_more flex-center">
+                            <img class="show-more-infos" src="/img/svg/plus.svg" alt="more-infos" width="20" height="20">
+                        </div>
                     </div>
-                </div>
-                <div class="card-actu flex item2">
-                    <div class="img-card"></div>
-                    <div class="text-bg_card flex">
-                        <p>Ici toutes les informations
-                            sur le Covid-19. Les mesures 
-                            a prendre sont désormais 
-                            appliquées. <br><br><br>
-                            Vous allez trouver un document ci dessous 
-                        </p>
-                        <div class="line"></div>
-                    </div>          
-                    <div class="btn_more flex-center">
-                        <img class="show-more-infos" src="/img/svg/plus.svg" alt="more-infos" width="20" height="20">
+                    <div class="card-actu flex item2">
+                        <div class="img-card"></div>
+                        <div class="text-bg_card flex">
+                            <p>Ici toutes les informations
+                                sur le Covid-19. Les mesures 
+                                a prendre sont désormais 
+                                appliquées. <br><br><br>
+                                Vous allez trouver un document ci dessous 
+                            </p>
+                            <div class="line"></div>
+                        </div>          
+                        <div class="btn_more flex-center">
+                            <img class="show-more-infos" src="/img/svg/plus.svg" alt="more-infos" width="20" height="20">
+                        </div>
                     </div>
-                </div>
-                <div class="card-actu flex item3">
-                    <div class="img-card"></div>
-                    <div class="text-bg_card flex">
-                        <p>Ici toutes les informations
-                            sur le Covid-19. Les mesures 
-                            a prendre sont désormais 
-                            appliquées. <br><br><br>
-                            Vous allez trouver un document ci dessous 
-                        </p>
-                        <div class="line"></div>
-                    </div>          
-                    <div class="btn_more flex-center">
-                        <img class="show-more-infos" src="/img/svg/plus.svg" alt="more-infos" width="20" height="20">
+                    <div class="card-actu flex item3">
+                        <div class="img-card"></div>
+                        <div class="text-bg_card flex">
+                            <p>Ici toutes les informations
+                                sur le Covid-19. Les mesures 
+                                a prendre sont désormais 
+                                appliquées. <br><br><br>
+                                Vous allez trouver un document ci dessous 
+                            </p>
+                            <div class="line"></div>
+                        </div>          
+                        <div class="btn_more flex-center">
+                            <img class="show-more-infos" src="/img/svg/plus.svg" alt="more-infos" width="20" height="20">
+                        </div>
                     </div>
-                </div>
-            </div>     
+                </div> 
+            </div>    
         </section>
         <!-- Bottom  -->
         <section class="bottom flex" id="insciptions-ecole-en-ligne">
