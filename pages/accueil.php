cfccf4f8f87bd7011b88f02edc190706432d9c8d
Felixoux <73909104+Felixoux@users.noreply.github.com>
2021-03-26 14:37:25 +0100

    Amelioration de la scroll-down__chevron

--- pages/accueil.php
+++ pages/accueil.php
@@ -135,7 +135,7 @@
                 </div>   
         </section>
         <div class="scroll-down__chevron center-mg">
-            <a href="#footer"><svg class="scroll-down__chevron" width='25' height='25' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill="#8491C7" d='M100 5.5c0 1.3-.5 2.6-1.5 3.5L50 57.5 1.5 9c-2-2-2-5.1 0-7.1s5.1-2 7.1 0L50 43.4 91.5 1.9c2-2 5.1-2 7.1 0 .9 1 1.4 2.3 1.4 3.6z'/></svg></a>
+            <a href="#actu"><svg class="scroll-down__chevron" width='25' height='25' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill="#8491C7" d='M100 5.5c0 1.3-.5 2.6-1.5 3.5L50 57.5 1.5 9c-2-2-2-5.1 0-7.1s5.1-2 7.1 0L50 43.4 91.5 1.9c2-2 5.1-2 7.1 0 .9 1 1.4 2.3 1.4 3.6z'/></svg></a>
         </div>
         <!-- Actu  -->
         <section class="actu" id="actu">  
