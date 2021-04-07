92c28feb5a203e5322e19fb0b20d714232aa0752
Felixoux <73909104+Felixoux@users.noreply.github.com>
2021-04-07 14:12:23 +0200

    creation de main pour tester le probleme de nav dans le menu du restaurant

--- pages/menu.php
+++ pages/menu.php
@@ -5,44 +5,46 @@
     <!-- nav  -->
     <?php require '../php/libs/header-navbar.php'; ?>
     <!-- main  -->
-    <section>
-        <h1 class="title">Menu du <span>05/10</span> au <span>09/10</span></h1>
-        <article class="menu">
-            <div class="menu__time flex">
-                <div class="flex-center w100"><h3>Lundi 5/10</h3></div>
-                <div class="flex-center w100"><h3>Mardi 6/10</h3></div>
-                <div class="flex-center w100"><h3>Jeudi 8/10</h3></div>
-                <div class="flex-center w100"><h3>Vendredi 9/10</h3></div>
-            </div>
-            <div class="menu__topic flex">
-                <div class="flex-center"><h3>Potage</h3></div>
-                <div class="flex-center"><h3>Plat principal</h3></div>
-                <div class="flex-center"><h3>Dessert</h3></div>
-            </div>
-            <div class="menu__meal flex stretch">
-                <div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
+    <main>
+        <section>
+            <h1 class="title">Menu du <span>05/10</span> au <span>09/10</span></h1>
+            <article class="menu">
+                <div class="menu__time flex">
+                    <div class="flex-center w100"><h3>Lundi 5/10</h3></div>
+                    <div class="flex-center w100"><h3>Mardi 6/10</h3></div>
+                    <div class="flex-center w100"><h3>Jeudi 8/10</h3></div>
+                    <div class="flex-center w100"><h3>Vendredi 9/10</h3></div>
                 </div>
-                <div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
+                <div class="menu__topic flex">
+                    <div class="flex-center"><h3>Potage</h3></div>
+                    <div class="flex-center"><h3>Plat principal</h3></div>
+                    <div class="flex-center"><h3>Dessert</h3></div>
                 </div>
-                <div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
+                <div class="menu__meal flex stretch">
+                    <div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                    </div>
+                    <div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                    </div>
+                    <div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                    </div>
+                    <div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                        <div><p>Chocolat chaud bien servi avec amour</p></div>
+                    </div>
                 </div>
-                <div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                    <div><p>Chocolat chaud bien servi avec amour</p></div>
-                </div>
-            </div>
-        </article>
-    </section>
+            </article>
+        </section>
+    </main>
     <!-- footer  -->
     <?php require '../php/libs/footer.php'; ?>
     <!-- script  -->
