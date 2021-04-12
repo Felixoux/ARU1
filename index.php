0b85b88be6f8d5c826b2cefd0c0cab98b853b03f
Felixoux <73909104+Felixoux@users.noreply.github.com>
2021-04-12 17:32:35 +0200

    Slider module + intégré sur index

--- index.php
+++ index.php
@@ -71,67 +71,77 @@
                 <button class="spoiler hidden">Afficher les contacts</button>
             </div>
         </div>
-        <div class="slider-infos">
+    </main>
+    <div class="slider-wrapper mb5">
             <h2 class="h3-medium mb3">Infos générale</h2>
-            <div class="slide-container">
+            <div class="slider-container">
                 <div>
                     <h5>Ecole fondamentale</h5>
-                    <p>Les photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbz</p>
+                    <p>
+                        Voici donc a quoi ressemblerait le slider d'infos comme demandé. Il est simple mais c'est déjà ca de fait, bien sur il n'est pas encore dynamique mais comme ca on a déjà une vue d'ensemble
+                    </p>
                     <a href="#">Aller vers le site</a>
                 </div>
                 <div>
                     <h5>Ecole Secondaire</h5>
                     <p>
-                        Les photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbzLes photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbzLes photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbzLes photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbzLes photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbzLes photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbzLes photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbzLes photos de classes ont été développée, cliquer ici pour acceder au site et voir plus d’information a ce propos je mange du caca auadfbsjbqbzdjbhqbdhjqbjzbdhbzjhqsbdjqbz
+                        Voici donc a quoi ressemblerait le slider d'infos comme demandé. Il est simple mais c'est déjà ca de fait, bien sur il n'est pas encore dynamique mais comme ca on a déjà une vue d'ensemble
                     </p>
                     <a href="#">Aller vers le site</a>
                 </div>
                 <div>
                     <h5>Internat</h5>
                     <p>
-                    Les photos ont bien été prises, voir la têtes de vos enfantsLes photos ont bien été prises, voir la têtes de vos enfantsLes photos ont bien été prises, voir la têtes de vos enfantsLes photos ont bien été prises, voir la têtes de vos enfants
+                        Voici donc a quoi ressemblerait le slider d'infos comme demandé. Il est simple mais c'est déjà ca de fait, bien sur il n'est pas encore dynamique mais comme ca on a déjà une vue d'ensemble
                     </p>
                     <a href="#">Aller vers le site</a>
                 </div>
-                <div></div>
+                <div>
+                    <h5>Ecole secondaire</h5>
+                    <p>
+                        Voici donc a quoi ressemblerait le slider d'infos comme demandé. Il est simple mais c'est déjà ca de fait, bien sur il n'est pas encore dynamique mais comme ca on a déjà une vue d'ensemble
+                    </p>
+                    <a href="#">Aller vers le site</a>
+                </div>
             </div>
         </div>
-    </main>
-    <footer class="index-footer hidden text-center">
-            <h3 class="h3-medium">Contacts</h3>
-            <div class="contact-1 py1">
-                <h4>Section fondamentale</h4>
-                <p>
-                    La Petite Ecole dans la prairie EFA ARU1 <br>
-                    72, avenue Paul Stroobant <br>
-                    1180 - Uccle
-                </p>
-                <p>Téléphone: 02/374.95.52 <br>
-                    Fax: 02/374.56.20
-                </p>
-            </div>
-            <div class="contact-2 py1">
-                <h4>Section Secondaire</h4>
-                <p>
-                    Athénée royal Uccle 1 <br>
-                    87, avenue Houzeau <br>
-                    1180 Bruxelles
-                </p>
-                <p>Téléphone : 02/ 374 05 84 <br>
-                    Fax : 02/ 375 28 91 <br>
-                    <a class="mail" href="mailto:secretariat@aru1.be">secretariat@aru1.be</a>
-                </p>
-            </div>
-            <div class="contact-3 py1">
-                <h4>Internat</h4>
-                <p>
-                    Internat annexé pour jeunes filles <br>
-                    66, Avenue du Vert Chasseur <br>
-                    1180 Uccle
-                </p>
-                <p>Téléphone : 02/ 374 51 07 <br>
-                    Fax : 02/ 372 38 42
-                </p>
+    <footer class="index-footer text-center">
+            <h3 class="h4-medium text-left my3">Contacts</h3>
+            <div class="index-footer__container">
+                <div class="contact-1 py1">
+                    <h4>Section fondamentale</h4>
+                    <p>
+                        La Petite Ecole dans la prairie EFA ARU1 <br>
+                        72, avenue Paul Stroobant <br>
+                        1180 - Uccle
+                    </p>
+                    <p>Téléphone: 02/374.95.52 <br>
+                        Fax: 02/374.56.20
+                    </p>
+                </div>
+                <div class="contact-2 py1">
+                    <h4>Section Secondaire</h4>
+                    <p>
+                        Athénée royal Uccle 1 <br>
+                        87, avenue Houzeau <br>
+                        1180 Bruxelles
+                    </p>
+                    <p>Téléphone : 02/ 374 05 84 <br>
+                        Fax : 02/ 375 28 91 <br>
+                        <a class="mail" href="mailto:secretariat@aru1.be">secretariat@aru1.be</a>
+                    </p>
+                </div>
+                <div class="contact-3 py1">
+                    <h4>Internat</h4>
+                    <p>
+                        Internat annexé pour jeunes filles <br>
+                        66, Avenue du Vert Chasseur <br>
+                        1180 Uccle
+                    </p>
+                    <p>Téléphone : 02/ 374 51 07 <br>
+                        Fax : 02/ 372 38 42
+                    </p>
+                </div>
             </div>
     </footer> 
     <script>
