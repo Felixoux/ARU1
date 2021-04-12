03b33c18d3dbf482c5f2404dd2221b5f7503f3ad
Felixoux <73909104+Felixoux@users.noreply.github.com>
2021-04-12 18:04:33 +0200

    Grille d'option refaite

--- pages/options.php
+++ pages/options.php
@@ -5,90 +5,80 @@
 <body id="g_options">
     <div class="page-wrapper">
         <?php require '../php/libs/header-navbar.php'; ?>
-        <header class="options-header">
-            <h1 class="mt-1 h1-big center-mg">Nos grilles d'options</h1>
+        <header class="options-header my3">
+            <h1 class="h1-big center-mg">Nos grilles d'options</h1>
         </header>
-        <main class="main-options mt-1">
-            <div class="card-options flex text-center">
-                <div class="card-options__img">                  
-                </div>
-                <div class="second">
-                    <h4 class="card__title pt-1">1ère année</h4>
-                    <button class="card__btn">
+        <main class="main-options mb5">
+            <div>
+                <img src="../img/grilles-options/1e.jpg" alt="1ère">
+                <h4 class="py1">1ère année</h4>
+                <button>
                         <a href="../docs/pdf/Grilles_1e.pdf">
                             <p>Lire la suite</p>
                         </a>
-                    </button>
-                </div>
-            </div>
-            <div class="card flex">
-                <div class="card-options__img">                  
-                </div>
-                <h4 class="card__title pt-1">1ère année immersion</h4>
-                <button class="card__btn">
-                    <a href="../docs/pdf/Grilles_1e_immersion.pdf">
-                        <p>Lire la suite</p>
-                    </a>
                 </button>
             </div>
-            <div class="card flex">
-                <div class="card-options__img">                  
-                </div>
-                <h4 class="card__title pt-1">2ème année</h4>
-                <button class="card__btn">
-                    <a href="../docs/pdf/Grilles_2e.pdf">
-                        <p>Lire la suite</p>
-                    </a>
+            <div>
+                <img src="../img/grilles-options/1e-immersion.jpg" alt="1ère">
+                <h4 class="py1">1ère année immersion</h4>
+                <button>
+                        <a href="../docs/pdf/Grilles_1e.pdf">
+                            <p>Lire la suite</p>
+                        </a>
                 </button>
             </div>
-            <div class="card flex">
-                <div class="card-options__img">                  
-                </div>
-                <h4 class="card__title line">2ème année<br>complémentaire</h4>
-                <button class="card__btn">
-                    <a href="../docs/pdf/Grilles_2e_complementaire.pdf">
-                        <p>Lire la suite</p>
-                    </a>
+            <div>
+                <img src="../img/grilles-options/2e.jpg" alt="1ère">
+                <h4 class="py1">2ème année</h4>
+                <button>
+                        <a href="../docs/pdf/Grilles_1e.pdf">
+                            <p>Lire la suite</p>
+                        </a>
                 </button>
             </div>
-            <div class="card flex">
-                <div class="card-options__img">                  
-                </div>
-                <h4 class="card__title pt-1">3ème année</h4>
-                <button class="card__btn">
-                    <a href="../docs/pdf/Grilles_3e.pdf">
-                        <p>Lire la suite</p>
-                    </a>
+            <div>
+                <img src="../img/grilles-options/2ecomplementaire.jpg" alt="1ère">
+                <h4 class="py1">2ème année complémentaire</h4>
+                <button>
+                        <a href="../docs/pdf/Grilles_1e.pdf">
+                            <p>Lire la suite</p>
+                        </a>
                 </button>
             </div>
-            <div class="card flex">
-                <div class="card-options__img">                  
-                </div>
-                <h4 class="card__title pt-1">4ème année</h4>
-                <button class="card__btn">
-                    <a href="../docs/pdf/Grilles_4e.pdf">
-                        <p>Lire la suite</p>
-                    </a>
+            <div>
+                <img src="../img/grilles-options/3e.jpg" alt="1ère">
+                <h4 class="py1">3ème année</h4>
+                <button>
+                        <a href="../docs/pdf/Grilles_1e.pdf">
+                            <p>Lire la suite</p>
+                        </a>
                 </button>
             </div>
-            <div class="card flex">
-                <div class="card-options__img">                  
-                </div>
-                <h4 class="card__title pt-1">5ème année</h4>
-                <button class="card__btn">
-                <a href="../docs/pdf/Grilles_5e.pdf">
-                    <p>Lire la suite</p>
-                </a>
+            <div>
+                <img src="../img/grilles-options/4e.jpg" alt="1ère">
+                <h4 class="py1">4ème année</h4>
+                <button>
+                        <a href="../docs/pdf/Grilles_1e.pdf">
+                            <p>Lire la suite</p>
+                        </a>
                 </button>
             </div>
-            <div class="card flex">
-                <div class="card-options__img">                  
-                </div>
-                <h4 class="card__title pt-1">6ème année</h4>
-                <button class="card__btn">
-                    <a href="../docs/pdf/Grilles_6e.pdf">
-                        <p>Lire la suite</p>
-                    </a>
+            <div>
+                <img src="../img/grilles-options/5e.jpg" alt="1ère">
+                <h4 class="py1">5ème année</h4>
+                <button>
+                        <a href="../docs/pdf/Grilles_1e.pdf">
+                            <p>Lire la suite</p>
+                        </a>
+                </button>
+            </div>
+            <div>
+                <img src="../img/grilles-options/6e.jpg" alt="1ère">
+                <h4 class="py1">6ère année</h4>
+                <button>
+                        <a href="../docs/pdf/Grilles_1e.pdf">
+                            <p>Lire la suite</p>
+                        </a>
                 </button>
             </div>
         </main>
